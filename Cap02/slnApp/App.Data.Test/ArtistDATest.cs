ac3dc67d3af91075e43fc84d0f2f3e36aac478dd
EXTENSION <kevin.alarcon.quispe@gmail.com>
2019-04-14 12:06:42 -0500

    Agregando el proyecto 1

---
+++ Cap02/slnApp/App.Data.Test/ArtistDATest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Data.Test
+{
+    [TestClass]
+    public class ArtistDATest
+    {
+        [TestMethod]
+        public void Count()
+        {
+            var da = new ArtistDA();
+
+            Assert.IsTrue(da.GetCount()>0);
+        }
+    }
+}
---
+++ Cap02/slnApp/App.Data/ArtistDA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data
+{
+    public class ArtistDA:BaseConnection
+    {
+        /// <summary>
+        /// Permite obtener la cantidad de registros
+        /// que existen en la tabla artista
+        /// </summary>
+        /// <returns>Retorna el número de registros</returns>
+        public int GetCount() {
+            var result = 0;
+            var sql = "SELECT COUNT(1) FROM Artist";
+            /*1.- Creando la instancia del objeto connection*/
+            using (IDbConnection cn=new SqlConnection(this.GetConnection))
+            {
+                /*2.- Creando el objeto command*/
+                IDbCommand cmd = new SqlCommand(sql);
+                cmd.Connection = cn;
+                cn.Open(); // Abriendo la conexion a la base de datos;
+
+                result = (int)cmd.ExecuteScalar();
+            }
+
+            return result;
+        }
+    }
+}
